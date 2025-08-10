b4a6c13f478038eae78e2a975495d1245ae22e46
Declan Ó F <declanofiaich@gmail.com>
2025-08-10 12:09:17 -0600

    Removing superfluous spacing not found in original. Adding pamphlets page.

--- LivingSober/chapter-27-letting-go-of-old-ideas.js
+++ LivingSober/chapter-27-letting-go-of-old-ideas.js
@@ -4,60 +4,39 @@ const chapter27 = {
 	"body": `<h1>27 Letting Go of Old Ideas</h1>
 <br>
 The ideas that got so deeply embedded in our lives during drinking do not all disappear quickly, as if by magic, the moment we start keeping the plug in the jug. Our days of wine and "Sweet Adeline" may be gone, but the malady lingers on.</p>
-<br>
 So we have found it therapeutic to nip off many old ideas that start to sprout up again. And they do, over and over.</p>
-<br>
 What we try to achieve is a feeling of being relaxed and freed from the bonds of our old thinking. Many of our former habits of thought and the ideas they produced, limit our freedom. They just weigh us down and are of no use—so it turns out when we look them over with a fresh eye. We don't&nbsp;<em>
 have</em>
 &nbsp;to hang on to them any longer unless, upon examination, they prove valid and still truly fruitful.</p>
-<br>
 We can now measure the present-day usefulness and truthfulness of a thought against a highly specific standard. We can say to ourselves, "Now, that is exactly what I used to think, in the drinking days. Does that kind of thinking help me stay sober? Is it good enough for me&nbsp;<em>
 today</em>
 ?"</p>
-<br>
 Many of our old ideas—especially those about alcohol, about drinking, about getting drunk, and about alcoholism (or problem drinking, if you prefer that term)—prove either worthless or actually self-destructive for us, and it is a great relief to get rid of them. Maybe a few examples will suffice to illustrate our willingness to throw out our old, useless ideas.</p>
-<br>
 For many of us as teen-agers, drinking was a way of proving that we were no longer children, or that we were manly, or sophisticated and wise, or tough enough to defy parents and other authorities. In many minds, drinking is closely tied in with romance, sex, and music, or with business success, wine snobbery, and jet-set luxury. If one is taught anything about drinking at school, it is often about dangers to health and the likelihood of losing a driver's license—not much else. And many people are still convinced that any drinking at all is immoral, leading straight to crime, suffering, disgrace,and death. Whatever our feelings may have been about drinking, positive or negative, they were often strong and more emotional than rational.</p>
-<br>
 Or our attitudes toward drinking may have been merely automatic, an unthinking acceptance of other people's opinions. To many, drinking is an essential part of social occasions—a harmless, convivial pastime done in certain places among friends at specific times. Others view drinking as a necessary accompaniment to eating. But now we ask ourselves:<em>
 &nbsp;Is</em>
 &nbsp;it actually impossible to enjoy friendship or food without chinking? Did our own way of drinking improve our social relationships? Did it heighten our appreciation of good food?</p>
-<br>
 The idea of getting drunk produces reactions even more extreme, pro or con. Getting tight is likely to be seen only as fun, or only as disgraceful. The very idea is repugnant to many people, on various grounds. To some of us, it was a desirable state, not only because it was expected of us by others and we liked the feeling, but also because it was a condition made light of by glamorous celebrities. Some people are intolerant of those who never get drunk at all; others are scornful of those who get&nbsp;<em>
 too</em>
 &nbsp;drunk. Modern-day health findings so far have had little influence on such attitudes.</p>
-<br>
 When we first heard the word "alcoholic," most of us associated it exclusively with older, unkempt, shaky, or unpleasant men we saw panhandling or passed out on skid rows. Well-informed people are now aware that such an idea is rubbish.</p>
-<br>
 Nevertheless, a residue of our ancient, muddy notions clung to many of us during our first attempts at sobriety. They blurred our vision and made it difficult to see the truth. But we finally became willing to entertain the thought that—just possibly—some of those ideas could be a bit erroneous, or at least no longer reflected accurately our own personal experience.</p>
-<br>
 When we could persuade ourselves to look at that experience honestly and to listen to ideas other than our own, we became open to a big array of information we had not examined carefully before.</p>
-<br>
 For instance, we could look at the scientific description: Alcohol is a drug that alters consciousness, not just a tasty thirst-quencher. The drug is found, we learned, not only in beverages, but also in some foods and medicines. And now, almost every day, we read or hear of a discovery that this particular drug does one more kind of physical damage (to the heart, the blood, the stomach, the liver, the mouth, the brain, etc.) not suspected before.</p>
-<br>
 Pharmacologists and other addictions experts now say that alcohol is not to be considered totally safe and harmless, whether used as beverage, stimulant, sedative, tonic, or tranquilizer. But it does not, of itself, necessarily lead straight to physical harm or mental degradation in every single case. Apparently, most people who use it can do so gracefully, without injury to themselves or others.</p>
-<br>
 Drinking, we found, can be viewed medically as ingestion of a drug; drunkenness, as overdosing. The misuse of this drug can, directly and indirectly, lead to problems of all sorts—physical, psychological, domestic, social, financial, vocational. Instead of thinking mostly about what drinking did&nbsp;<em>
 for</em>
 &nbsp;us, we began to see what it does&nbsp;<em>
 to</em>
 &nbsp;some people.</p>
-<br>
 We have found out that&nbsp;<em>
 anybody</em>
 &nbsp;who has trouble of any sort related to drinking may have the condition called "alcoholism." This illness strikes without regard for age, creed, sex, intelligence, ethnic background, emotional health, occupation, family situation, strong constitution, eating habits, social or economic status, or general character. It is not a question of how much or how you drink, or when, or why, but of how your drinking affects your life—what happens when you drink.</p>
-<br>
 Before we could recognize the illness in ourselves, we had to unload this tired old myth: It would be a sign of shameful weakness to admit that we couldn't handle the sauce any more (if we ever could).</p>
-<br>
 Weakness? Actually, it takes considerable courage to stare unblinkingly at the hard truth, sparing nothing, without glossing over anything, without excuses, and without kidding ourselves. (It is unseemly to brag, but frankly, many of us think that at kidding ourselves we were world champions.)</p>
-<br>
 The process of recovery from alcoholism also has been clouded with misconceptions. Like millions of others who have watched a person drinking himself or herself to death, we have wondered why &nbsp;the drinker did not use willpower to stop drinking. That is another outdated idea, but it sticks because many of us have been exposed early in life to some model of super willpower. Maybe there was the family or neighborhood legend of good old Uncle John. Known as a rake and a heller for years, he suddenly gave up wine, women, and song at age 50 and became a model of propriety and rectitude who never touched another drop.</p>
-<br>
 The childish notion that we can do likewise when we get ready is a dangerous delusion. We are not anybody else. We are only ourselves. (We are not Grandpa, who drank a fifth a day until he was 90, either.)</p>
-<br>
 It is now well established that willpower all by itself is about as effective a cure for alcohol addiction as it is for cancer. Our own experience has verified that repeatedly. Most of us tried going it alone, hoping either to control our drinking or to stop, and we had no lasting success in either endeavor. Even so, it wasn't easy to admit we needed help. That, too, looked like a sign of weakness. Yes, we were being taken in by another myth.</p>
-<br>
 But we finally asked ourselves: Wouldn't it be more intelligent to seek out and tap a strength greater than our own than to persist in our futile solo efforts, after they had time and again been proved ineffective? We still don't think it is very smart to keep trying to see in the dark if you can simply switch on a lamp and use its light We didn't get sober entirely on our own. That isn't the way we learned to stay sober. And the full enjoyment of living sober isn't a one-person job, either.</p>
-<br>
 When we could look, even temporarily, at just a few new ideas different from our old ones, we had already begun to make a sturdy start toward a happy, healthier new life. It happened just that way to thousands and thousands of us who deeply believed it never could.</p>`
 };
